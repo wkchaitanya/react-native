20b254e2c50cc70961c0e094fcf7d70493c954c7
wkchaitanya <54983245+wkchaitanya@users.noreply.github.com>
2020-11-16 23:58:12 +0530

    Expo Splash screen commit

--- ExpoSplashScreenPackage/App.tsx
+++ ExpoSplashScreenPackage/App.tsx
@@ -1,1 +1,32 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
+export default function App() {
+  useEffect(() => {
+    try {
+      SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    }
+
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 20000);
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
